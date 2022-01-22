e85519967114d326db4bc0832047597c4b7777aa
ASUS WSL <yacovw@gmail.com>
2022-01-22 20:32:50 +0200

    Remove build directory from repository Add Double class

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yaki/Projects/Inbar/Ex1
-# Build directory: /home/yaki/Projects/Inbar/Ex1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
